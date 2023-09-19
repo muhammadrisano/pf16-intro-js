a7db7ac04197d78185f960495deb79f2fc13dcf6
muhammadrisano <muhammadrisano@gmail.com>
2023-09-19 10:31:52 +0700

    add part5

---
+++ part5/pembahasan.js
@@ -0,0 +1,50 @@
+const biodata = {
+    name: "risano",
+    age: 17,
+    email: "risano@gmail.com",
+    tempatLahir: "bandung",
+    hobies: ["ngoding", "joging", "gaming"],
+    // printNama: (nama)=>{
+    //    console.log(nama);
+    // },
+    // printEmail: (email)=>{
+    //     console.log(email);
+    // }
+
+  };
+
+
+
+const simpanData = (key, value) =>{
+
+    biodata[key] = value
+    // logic
+
+    // bonus
+    // silahka coba menggunakan spread operator
+}
+
+
+// simpanData('printEmail', )
+
+// soal 2
+
+const ReturnDays= ()=>{
+    // logic
+}
+
+const ReturnFirstName =()=>{
+    // logic
+}
+
+const greeting = (clock, functionReturnDay, fullName,functionReturnFirstName)=>{
+
+    // const resultDay = functionReturnDay()
+    // const resultFistname = functionReturnFirstName()
+    // console.log('')
+    // return
+}
+
+greeting(20, ReturnDays, 'risano akbar', ReturnFirstName)
+
+// “Selamat Malam, risano!”
